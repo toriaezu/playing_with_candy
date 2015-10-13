2815851f0b2b88fca0817d00ff4075458808c828
toriaezu <simon.krollpfeifer@gmail.com>
2015-10-13 14:06:31 -0700

    Update default.rb

--- candy_bar/recipes/default.rb
+++ candy_bar/recipes/default.rb
@@ -7,5 +7,5 @@
 # All rights reserved - Do Not Redistribute
 
 file "/tmp/banzai" do
-  content search(:aws_opsworks_volumes, "mount_point:/vol/ebi")
+  content search(:aws_opsworks_volume, "mount_point:/vol/ebi")
 end
