bceaeab7935cb8b317fce93a7125637bdb03e07f
Simon Krollpfeifer <simonkro@amazon.com>
2015-10-13 22:24:37 -0700

    add volume access chef 11.10

--- candy_bar/recipes/volumes_chef1110.rb
+++ candy_bar/recipes/volumes_chef1110.rb
@@ -1,2 +1,8 @@
 
 require "pp"
+
+current_node = search(:aws_opsworks_instance, "self:true").first
+layer_id = current_node["layer_ids"].first
+volume_configurations = search(:aws_opsworks_layer, "layer_id:#{layer_id}").first["volume_configurations"]
+
+pp volume_configurations
