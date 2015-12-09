343b1f8240789cadb939a019eb25ea3d8675f168
toriaezu <simon.krollpfeifer@gmail.com>
2015-12-09 19:56:42 +0100

    Update pineapple.rb

--- candy_bar/recipes/pineapple.rb
+++ candy_bar/recipes/pineapple.rb
@@ -2,23 +2,28 @@
 
 def ticktick
   instance = search("aws_opsworks_instance", "self:true").first 
-  puts "those are my layers #{instance[:layer_ids].first}"
-  puts "my hostname wooop #{instance[:hostname]}"
-  puts "whatever #{instance[:public_ip]}"
+  Chef::Log.info "those are my layers #{instance[:layer_ids].first}"
+  Chef::Log.info "my hostname wooop #{instance[:hostname]}"
+  Chef::Log.info "whatever #{instance[:public_ip]}"
 
-  puts "the available rest #{instance.inspect}"
+  Chef::Log.info "the available rest #{instance.inspect}"
 rescue => e
-  puts "nah nah that is not polite #{e.backtrace.join("\n")} -- #{e.message}"
+  Chef::Log.info "nah nah that is not polite #{e.backtrace.join("\n")} -- #{e.message}"
 end
 
 def beer_on_a_stick
   search_string = "hostname:`hostname`"
   instance = search("aws_opsworks_instance", search_string).first 
   
-  puts "beer on a stick returns #{instance.inspect}"
+  Chef::Log.info "beer on a stick returns #{instance.inspect}"
 rescue => e
-  puts "nah nah that is not polite #{e.backtrace.join("\n")} -- #{e.message}"
+  Chef::Log.info "nah nah that is not polite #{e.backtrace.join("\n")} -- #{e.message}"
 end
+
+search_string = "hostname:`hostname`"
+instance = search("aws_opsworks_instance", search_string).first 
+  
+Chef::Log.info "beer on a stick returns #{instance.inspect}"
 
 ticktick
 beer_on_a_stick
